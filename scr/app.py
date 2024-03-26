9343713d53620196718916d0b923789b5b185449
Juan Casas Lopez <juancl@protonmail.com>
2024-03-26 14:53:07 +0100

    Actualizo CORS

--- scr/app.py
+++ scr/app.py
@@ -22,7 +22,7 @@ import json
 
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/api/*": {"origins": ["https://develop--despliegueprueba.netlify.app", "https://otrodominio.com"]}})  # Agrega múltiples dominios a la lista blanca de CORS
 
 #===============================================================================================================================
 # Endpoint HOME 
